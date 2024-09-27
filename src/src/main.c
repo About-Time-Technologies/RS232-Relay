d61a7938df60c8a9ef4f9a248e93e6661c5f524c
Sam Lane <theminordeveloper@outlook.com>
2024-09-27 19:44:42 +0100

    Reverting to a really simple codebase

---
+++ src/src/main.c
@@ -0,0 +1,40 @@
+/*  WiFi softAP Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+
+static const char *TAG = "RS232-Relay";
+
+
+void app_main(void)
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    ESP_LOGI(TAG, "Initialised");
+    
+    ESP_LOGW(TAG, "Testing");
+}
--- src_old/python/main.py
+++
@@ -1,7 +0,0 @@
-import serial
-
-ser = serial.Serial('COM4', 115200)
-ser.write(b"\r")
-ser.close()
-
-
